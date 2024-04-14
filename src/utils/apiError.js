b22bff4ab88e0b839996bf03b7fb75624e6aa39b
adityaraskar1001 <adityaraskar10@gmail.com>
2024-04-15 01:42:00 +0530

    sixth commit

---
+++ src/utils/apiError.js
@@ -0,0 +1,23 @@
+class apiError extends Error{
+    constructor(
+        statusCode,
+        message = "Something went wrong",
+        errors = [],
+        stackh = ""
+    ){
+        super(message)
+        this.statusCode = statusCode
+        this.data = null,
+        this.message = message,
+        this.success = false,
+        this.errors = errors
+  
+        if(stackh){
+            this.stackh = stackh
+        }else{
+            Error.captureStackTrace(this,this.constructor)
+        }
+    }
+}
+
+export {apiError}
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,20 @@
+import { Promise } from "mongoose"
+
+const asyncHandler = (requestHandler) =>{
+    (req, res, next) =>{
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+// const asyncHandler = (requestHandler) => async(req, res, next) => {
+//     try {
+        
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
+
+export {asyncHandler}
