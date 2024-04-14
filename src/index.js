558acfc4e62530e065ff199aba94c0abd913f8a0
adityaraskar1001 <adityaraskar10@gmail.com>
2024-04-15 00:39:31 +0530

    fifth commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path: './.env'
+})
+
+connectDB()
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express()
+// (async() => {
+//     try {
+//         await mongoose.connect(`${process.env.MONGO_DB_URI}/${DB_NAME}`)
+//         app.on("error", (error) => {
+//             console.error("ERROR: ",error);
+//             throw error
+//         })
+//     } catch (error) {
+//         console.error("ERROR: ", error);
+//         throw err
+//     }
+// })()
